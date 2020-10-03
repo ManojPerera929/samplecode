7d15d6234648f29eb3a197b7b3e7d7eac4cc49f5
Chamil <chamiljay88@gmail.com>
2020-10-03 18:30:51 +0530

    Add all files

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform,StyleSheet,Text,View,Image} from 'react-native';
+
+export default class App extends Component{
+
+  render(){
+    
+    return(
+      
+      <View>
+        <Text>He</Text>
+      </View>
+    );
+  }
+
+}
+
